b1832e4d0831101a59cd1023b9b34f16a9c4f927
Greg Lownes <glownes@whitelabelcomm.com>
2025-03-08 10:42:40 -0500

    react-router-dom, basic routing with home and not found

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,15 @@
-import "./App.css"
-import { Counter } from "./features/counter/Counter"
-import { Quotes } from "./features/quotes/Quotes"
-import logo from "./logo.svg"
+import { Routes, Route } from "react-router-dom";
+// import "./App.css"
+
+import Home from "./page/Home";
+import NotFound from "./page/NotFound";
 
 const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <Quotes />
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://reselect.js.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Reselect
-          </a>
-        </span>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
   )
 }
 
---
+++ src/layout/Layout.tsx
@@ -0,0 +1,35 @@
+// import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+// import { useAppSelector } from "../app/hooks";
+
+type LayoutProps = {
+  children: React.ReactNode;
+};
+
+export const Layout = (props: LayoutProps) => {
+  // const defaultAccountID = useAppSelector(
+  //   (state) => state.app.defaultAccountID
+  // );
+  // const defaultAccountIDShort = defaultAccountID.slice(0, 6);
+  // const defaultAccountName = useAppSelector(
+  //   (state) => state.app.defaultAccountName
+  // );
+
+  // const [selectedUseCase, setSelectedUseCase] = useState("");
+  // const navigate = useNavigate();
+
+  // const handleUseCaseChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+  //   const selectedOption = event.target.value;
+  //   navigate(selectedOption);
+  //   setSelectedUseCase(selectedOption);
+  // };
+
+  return (
+    <div style={{ margin: "5px 10px 10px" }}>
+      <Link to="/">Home</Link>
+      &nbsp;&nbsp;&nbsp;
+      <hr />
+      {props.children}
+    </div>
+  );
+};
