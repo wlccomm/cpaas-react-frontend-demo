770013a3d758a1fc0b135e77d23dd2431ca99c34
Greg Lownes <glownes@whitelabelcomm.com>
2025-03-09 08:26:26 -0400

    fix lint issue in code

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,5 +1,6 @@
 // import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+// import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 // import { useAppSelector } from "../app/hooks";
 
 type LayoutProps = {
