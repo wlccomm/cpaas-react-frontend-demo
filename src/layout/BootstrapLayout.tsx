ecca6b2382dce6c7fdd99fe8bfa8d986318eeeee
Greg Lownes <glownes@whitelabelcomm.com>
2025-03-10 13:01:55 -0400

    add bootstrap top navbar

--- src/layout/BootstrapLayout.tsx
+++ src/layout/BootstrapLayout.tsx
@@ -3,17 +3,41 @@ import { Link } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown'
 
 export const BootstrapLayout = (props: LayoutProps) => {
   return (
     <Container fluid="lg">
       <Row>
+        <Navbar expand="lg" bg="primary" data-bs-theme="dark">
+          <Container>
+            <Navbar.Brand as={Link} to="/" >CPaaS Demo App</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link as={Link} to="/" >Home</Nav.Link>
+                <Nav.Link as={Link} to="/bootstrap" >Bootstrap</Nav.Link>
+                <Nav.Link href="#link">Link</Nav.Link>
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.2">
+                    Another action
+                  </NavDropdown.Item>
+                  <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item href="#action/3.4">
+                    Separated link
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Row>
+      <Row>
         <Col>
-          <Link to="/">Home</Link>
-          &nbsp;&nbsp;&nbsp;
-          <Link to="/bootstrap">Bootstrap</Link>
-          &nbsp;&nbsp;&nbsp;
-          <hr />
           {props.children}
         </Col>
       </Row>
